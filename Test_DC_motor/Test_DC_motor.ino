926acf818f536231a855ff99ec263502c5367764
Chaoyang Liu <chaoyanglius@gmail.com>
2016-07-22 20:57:00 +0800

    Add project files.

---
+++ Test_DC_motor/Test_DC_motor.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		Test_DC_motor.ino
+ Created:	7/22/2016 8:56:52 PM
+ Author:	bird
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "Test_DC_motorLib.h"
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Test_DC_motor/src/_micro-api/libraries/Test_DC_motorLib/src/Test_DC_motorLib.h
@@ -0,0 +1,19 @@
+/*
+ Name:		Test_DC_motorLib.h
+ Created:	7/22/2016 8:56:52 PM
+ Author:	bird
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _Test_DC_motorLib_h
+#define _Test_DC_motorLib_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
